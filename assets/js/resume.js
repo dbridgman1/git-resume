401f7b6e0d67730550c57087ddf7eb2591f991ea
Alex Marzullo <marzullo94@gmail.com>
2020-12-12 11:12:53 -0500

    Updated resumes Files with API format

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,12 +1,35 @@
 window.onload = function() {
-alert("meow")
+var proList    = document.querySelectorAll(".proLink")
+var userInput  = document.querySelector("#icon_telephone")
+var meow1 = document.querySelectorAll(".meow1")
+userInput = "amarz94"
 function resumeLoad() {
-    var gitUser = "https://api.github.com/users/davidsaulrodriguez"
+    var gitUser = "https://api.github.com/users/" + userInput
+    var gitRepo = "https://api.github.com/users/" + userInput + "/repos"
     $.ajax({
         url: gitUser,
         method: "GET"
     }).then(function(response){
         console.log(response)
+        document.getElementById("introText").innerHTML = "My name is " + response.name + ", and I am a software developer"
+        document.getElementById("bioText").innerHTML = response.bio
+        $("img").attr("src", response.avatar_url)
+        $("img")
+        .css('width', 'auto')
+        .css('max-width', '250px')
+        .css('height', 'auto')
+        .css('max-height', '300px')
+        $.ajax({
+            url: gitRepo,
+            method: "GET"
+        }).then(function(response){
+            for(i = 0; i < proList.length; i++){
+                var projectLink = document.createElement("li")
+                proList[i].innerHTML = ""
+                projectLink.innerHTML = "<a href=" + response[i].html_url + ">" + response[i].html_url + "</a>"
+                proList[i].append(projectLink)
+            }
+        })
     })
 }
 resumeLoad();
